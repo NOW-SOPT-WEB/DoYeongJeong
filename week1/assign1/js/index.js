9d61c8d19d868c53210c8c25ac655af0b529ec5c
jerry <becomegoodguy@gmail.com>
2024-04-14 22:17:39 +0900

    refactor: sideNav 애니메이션 분리

--- week1/assign1/js/index.js
+++ week1/assign1/js/index.js
@@ -1,17 +1,5 @@
 import { SHOPPING_LIST } from "./const.js";
 
-const hamburger = document.getElementById("hamburger");
-const navClose = document.getElementsByClassName("nav_close")[0];
-const navBar = document.getElementsByClassName("side_nav")[0];
-
-hamburger.addEventListener("click", () => {
-  navBar.style.transform = "translateX(0)";
-});
-
-navClose.addEventListener("click", () => {
-  navBar.style.transform = "translateX(200px)";
-});
-
 const section = document.querySelector("#list");
 
 const categoryName = document.createElement("h2");
