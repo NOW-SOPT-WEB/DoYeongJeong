2dc3efeabc2cdf670ef3844ab0c029a0e07d9a26
jerry <becomegoodguy@gmail.com>
2024-04-29 18:13:56 +0900

    feat: 카드 리스트 추가

---
+++ week3/src/shared/constants/Card.js
@@ -0,0 +1,38 @@
+export const CardList = [
+  {
+    id: 1,
+    img: 'public/images/루피1.png',
+  },
+  {
+    id: 2,
+    img: 'public/images/루피2.png',
+  },
+  {
+    id: 3,
+    img: 'public/images/루피3.png',
+  },
+  {
+    id: 4,
+    img: 'public/images/루피4.png',
+  },
+  {
+    id: 5,
+    img: 'public/images/루피5.png',
+  },
+  {
+    id: 6,
+    img: 'public/images/루피6.png',
+  },
+  {
+    id: 7,
+    img: 'public/images/루피7.png',
+  },
+  {
+    id: 8,
+    img: 'public/images/루피8.png',
+  },
+  {
+    id: 9,
+    img: 'public/images/루피9.png',
+  },
+];
