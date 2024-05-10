76992a0ea4831003147844306ed8711f1cddef56
jerry <becomegoodguy@gmail.com>
2024-05-10 15:23:52 +0900

    fix: 전화번호 정규 표현식 유틸함수로 분리 (#5)

--- week4/src/utils/index.ts
+++ week4/src/utils/index.ts
@@ -1,5 +1,16 @@
-export const validatePassword = (password: string) => {
+export const validatePassword = (password: string): boolean => {
   const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
 
   return regex.test(password);
+};
+
+export const formatPhoneNumber = (prevPhoneNumber: string): string => {
+  const newNumber = prevPhoneNumber;
+
+  if ((newNumber.length === 4 || newNumber.length === 9) && newNumber[newNumber.length - 1] !== '-') {
+    return newNumber.slice(0, newNumber.length - 1) + '-' + newNumber[newNumber.length - 1];
+  } else if ((newNumber.length === 4 || newNumber.length === 9) && newNumber[newNumber.length - 1] === '-') {
+    return newNumber.slice(0, newNumber.length - 1);
+  }
+  return newNumber;
 };
