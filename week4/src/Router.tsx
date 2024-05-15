a17bef394f4885fc0a364539c5583e8d30f475c8
jerry <becomegoodguy@gmail.com>
2024-05-15 22:12:55 +0900

    refactor: 라우터 간결하게 변경

--- week4/src/Router.tsx
+++ week4/src/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import Home from './pages/home/Home';
 import Main from './pages/main/Main';
@@ -6,7 +6,7 @@ import Mypage from './pages/mypage/Mypage';
 import Signup from './pages/signup/Signup';
 
 const Router = () => {
-  const routerData = [
+  const routerData = createBrowserRouter([
     {
       path: '/',
       element: <Home />,
@@ -23,17 +23,9 @@ const Router = () => {
       path: '/mypage/:id',
       element: <Mypage />,
     },
-  ];
+  ]);
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        {routerData.map((data, i) => (
-          <Route key={`route-${i}`} path={data.path} element={data.element} />
-        ))}
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={routerData}></RouterProvider>;
 };
 
 export default Router;
