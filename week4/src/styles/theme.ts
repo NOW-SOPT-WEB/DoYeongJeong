2043b158d7577c423044e72b771aa554a108e548
jerry <becomegoodguy@gmail.com>
2024-05-15 22:25:35 +0900

    style: theme type 추가

--- week4/src/styles/theme.ts
+++ week4/src/styles/theme.ts
@@ -3,7 +3,10 @@ export const theme = {
     primary: '#000000',
     secondary: '#ffffff',
     tertiary: '#ff0000',
+    blue: '#007bff',
+    deepBlue: '#0056b3',
   },
 };
 
+export type ColorType = typeof theme.color;
 export default theme;
